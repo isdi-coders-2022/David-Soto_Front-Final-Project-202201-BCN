fbffedc46a45ef96f3971157e313d9162ac6d5b4
David Soto <davidsotosendarrubias@gmail.com>
2022-03-15 09:52:30 +0100

    Created redux store

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import "./styles/global.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./redux/store/index";
 
 ReactDOM.render(
   <React.StrictMode>
