c960a1323be777886e2fc2a97d13113ff99d61bb
David Soto <davidsotosendarrubias@gmail.com>
2022-03-23 16:47:11 +0100

    Added thunks tests

--- src/redux/thunks/thunks.test.ts
+++ src/redux/thunks/thunks.test.ts
@@ -1,4 +1,5 @@
-import { loadGlobalListThunk } from "./thunks";
+import CreatedHero from "../../interfaces/CreatedHero";
+import { loadCreatedHeroThunk, loadGlobalListThunk } from "./thunks";
 
 describe("Given a loadGlobalListThunk", () => {
   describe("When it's called", () => {
@@ -59,6 +60,73 @@ describe("Given a loadGlobalListThunk", () => {
       const getState = jest.fn();
 
       await loadGlobalListThunk(dispatch, getState, undefined);
+
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
+
+describe("Given a loadCreatedHeroThunk", () => {
+  describe("When it's called", () => {
+    test("Then it should call dispatch function", async () => {
+      jest.setTimeout(9000);
+
+      const expectedAction = {
+        heroes: {
+          heroes: [
+            {
+              id: 12,
+              name: "TestingHero",
+              slug: "string",
+              powerstats: {
+                intelligence: 1,
+                strength: 1,
+                speed: 1,
+                durability: 1,
+                power: 1,
+                combat: 1,
+              },
+              appearance: {
+                gender: "no binary",
+                race: "Superior Testig Race",
+                height: ["1m", "1m"],
+                weight: ["1m", "1m"],
+                eyeColor: "Black",
+                hairColor: "Rainbow",
+              },
+              biography: {
+                fullName: "Sir Test Von Ing Hero",
+                alterEgos: "test",
+                aliases: ["test"],
+                placeOfBirth: "jestTesting",
+                firstAppearance: "In HeroCard Test",
+                publisher: "DavidSoto",
+                alignment: "Hopefully good enought",
+              },
+              work: {
+                occupation: "Testing Guy",
+                base: "Jest House",
+              },
+              connections: {
+                groupAffiliation: "Jest Group",
+                relatives: "Someone",
+              },
+              images: {
+                xs: "IMG",
+                sm: "IMG",
+                md: "IMG",
+                lg: "IMG",
+              },
+            },
+          ],
+        },
+        type: "load-global-list",
+      };
+
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+
+      await loadCreatedHeroThunk(dispatch, getState);
 
       expect(dispatch).toHaveBeenCalledWith(expectedAction);
     });
